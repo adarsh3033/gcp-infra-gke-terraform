d4275949b4eca0711fca66e5fa19df3b13ff2f9d
raman kumar singh <rs5395913@gmail.com>
2025-06-20 02:24:37 +0530

    Add Terraform GCP infra with GitHub Actions

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "devops-vpc"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "devops-cluster"
+  location = var.region
+
+  network = google_compute_network.vpc_network.name
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
