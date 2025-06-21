c68613e776ca13e8eaa8e61c08bf0919a0ef6fb1
raman kumar singh <rs5395913@gmail.com>
2025-06-21 14:58:50 +0530

    updated disk size

--- main.tf
+++ main.tf
@@ -18,6 +18,8 @@ resource "google_container_cluster" "primary" {
 
   node_config {
     machine_type = "e2-medium"
+     disk_type      = "pd-standard"   
+    disk_size_gb   = 50 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
